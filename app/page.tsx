97899d659cb904139eb04bbdea7d6b4517a61332
mankeylol <mankeyooooo@gmail.com>
2025-08-14 20:08:39 +0530

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,18 @@ const bungeeFont = Bungee({ weight: "400" });
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[#FF5400]">
-      <img src="btc-overlay.png" className="w-[40vw] h-[80vh] absolute opacity-58" alt="btc-overlay" />
-      <h1 className="text-[64px]/15 z-1 w-[50px] mr-[300px] mt-[250px] -rotate-15 tracking-[.2em]">WIN ROYAL</h1>
-      <div className="w-64 h-90 bg-[#2D2828] opacity-64 [clip-path:polygon(0_30%,100%_0,100%_100%,0%_100%)] rounded-[41px]"></div>
+    <div className="font-sans items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 bg-[#FF5400]">
+      <img src="btc-overlay.png" className="w-[40vw] h-[80vh] absolute opacity-58 top-[20px]" alt="btc-overlay" />
+      <h1 className="text-[64px]/15 z-1 w-[50px] mr-[300px] mt-[20px] -rotate-15 tracking-[.2em]">WIN ROYAL</h1>
+      <img src="hero-bg-blur.svg" className="w-[41vw] h-[66vh] absolute opacity-64 mix-blend-difference top-[230px] left-[430px]" alt="btc-overlay" />
+      <div className="z-2 mt-[140px] isolate text-[38px]/18">
+      <p className="m-0 p-[0px] z-2 ">FULLY ON CHAIN</p>
+      <p className="m-0 p-[0px] z-2 ">DECENTRALIZED</p>
+      <p className="m-0 p-[0px] z-2 ">INSTANT</p>
+      <button className="z-2 mt-[20px] isolate text-[15px] bg-white text-[#141414] ml-[20px] w-[270px] h-[50px] flex items-center justify-center rounded-[50px] cursor-pointer">
+        Type email
+      </button>
+      </div>
     </div>
   );
 }
